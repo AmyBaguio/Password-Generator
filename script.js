4b7ded676bd1ca1d13c9b6278664f6f7a616d678
amalyn <amalynlaquinta@yahoo.com>
2020-09-11 21:23:10 +0800

    my first upload with opportunities for revision

---
+++ script.js
@@ -0,0 +1,158 @@
+// Add event listener to the generate button
+document.getElementById("generate").addEventListener("click", function () {
+
+  // ARRAY OF //
+  var lowercaseAlphabetChar = ["abcdefghijklmnopqrstuvwxyz"];
+  var uppercaseAlphabetChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var numericChar = ["0123456789"];
+  var specialChar = ["!@#$%^&*?~/|:'><"];
+  var password = "";
+  var pSelection = "";
+
+  // User Prompts
+  var length = prompt("Password must be 8 to 128 Characters. How long is your password?");
+
+  // Making sure User Chooses between 8 and 128 Characters
+  if (length < 8 || length > 128) {
+    alert("Length has to be Between 8 and 128. Please Try Again")
+    return
+  }
+  var wantsLower = confirm("Do you want Lowercase Characters?")
+  var wantsUpper = confirm("Do you want Uppercase Characters?")
+  var wantsNumbers = confirm("Do you want Numerical Characters?")
+  var wantsSpecial = confirm("Do you want Special Characters?")
+
+  // This functtion gets a RANDOM ITEM from an array//
+
+  // Function for LowerCase
+  function random_item(lowercaseAlphabetChar) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+  var items = ["abcdefghijklmnopqrstuvwxyz"];
+  console.log(random_item(lowercaseAlphabetChar));
+
+
+  // Function for UpperCase
+  function random_item(uppercaseAlphabetChar) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+  var items = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  console.log(random_item(uppercaseAlphabetChar));
+
+  //Function for Number
+  function random_item(numericChar) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+  var items = ["0123456789"];
+  console.log(random_item(numericChar));
+
+  // Function for Special Character
+  function random_item(specialChar) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+  var items = ["!@#$%^&*?~/|:'><"];
+  console.log(random_item(specialChar));
+
+  // Build String in an Array //
+  var randomLowerCase = random_item(lowercaseAlphabetChar)
+  var randomUpperCase = random_item(uppercaseAlphabetChar)
+  var randomNumber = random_item(numericChar)
+  var randomSpecialChar = random_item(specialChar)
+  console.log(`${randomLowerCase} ${randomUpperCase} ${randomNumber} ${randomSpecialChar}.`)
+
+  //Verification Points System -One point is allocated when an option is confirmed. If no points are allocated, user has not selected anything.
+  var verify = 0;
+
+  if (wantsLower == true || wantsUpper == true || wantsNumbers == true || wantsSpecial == true) {
+    verify += 1;
+  }
+  if (verify == 0) {
+    alert("You have not selected a character Type, Please Try again.")
+    return
+  }
+
+// Script will exit if not verified, if verified we Proceed to Grouping Password to User input.
+  function generatePWD() {
+    for (var i = 0; i <= length; i++) {
+      password = password + pSelection.charAt(Math.floor(Math.random() * Math.floor(pSelection.length - 1)));
+    }
+  }
+
+  // IF STATEMENTS if User Prefers LowerCase
+  if (wantsLower && !wantsUpper && !wantsNumbers && !wantsSpecial) {
+    pSelection = lowercaseAlphabetChar;
+    generatePWD();
+  }
+  // LowerCase and UpperCase
+  else if (wantsLower && wantsUpper && !wantsNumbers && !wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + uppercaseAlphabetChar;
+    generatePWD();
+  }
+
+  //LowerCase and UpperCase and Numbers
+  else if (wantsLower && wantsUpper && wantsNumbers && !wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + uppercaseAlphabetChar + numericChar;
+    generatePWD();
+  }
+  // Everything
+  else if (wantsLower && wantsUpper && wantsNumbers && wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + uppercaseAlphabetChar + numericChar + specialChar;
+    generatePWD();
+  }
+  //Lowercase and Numbers
+  else if (wantsLower && !wantsUpper && wantsNumbers && !wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + numericChar;
+    generatePWD();
+  }
+  //Lowercase and Numbers and Special
+  else if (wantsLower && !wantsUpper && wantsNumbers && wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + numericChar + specialChar;
+    generatePWD();
+  }
+  // Lowercase and Special
+  else if (wantsLower && !wantsUpper && !wantsNumbers && wantsSpecial) {
+    pSelection = lowercaseAlphabetChar + specialChar;
+    generatePWD();
+  }
+  // Uppercase
+  else if (!wantsLower && wantsUpper && !wantsNumbers && !wantsSpecial) {
+    pSelection = uppercaseAlphabetChar;
+    generatePWD();
+  }
+  // UpperCase and Numbers
+  else if (!wantsLower && wantsUpper && wantsNumbers && !wantsSpecial) {
+    pSelection = uppercaseAlphabetChar + numericChar;
+    generatePWD();
+  }
+
+  // Uppercase and Numbers and Special
+  else if (!wantsLower && wantsUpper && wantsNumbers && wantsSpecial) {
+    pSelection = uppercaseAlphabetChar + numericChar + specialChar;
+    generatePWD();
+  }
+  // Uppercase and Special
+  else if (!wantsLower && wantsUpper && !wantsNumbers && wantsSpecial) {
+    pSelection = uppercaseAlphabetChar + specialChar;
+    generatePWD();
+  }
+  // Numbers
+  else if (!wantsLower && !wantsUpper && wantsNumbers && !wantsSpecial) {
+    pSelection = numericChar;
+    generatePWD();
+  }
+  // Numbers and Special
+  else if (!wantsLower && !wantsUpper && wantsNumbers && wantsSpecial) {
+    pSelection = numericChar + specialChar;
+    generatePWD();
+  }
+  // Special
+  else if (!wantsLower && !wantsUpper && !wantsNumbers && wantsSpecial) {
+    pSelection = specialChar;
+    generatePWD();
+  }
+
+  document.getElementById("password").innerHTML = password;
+  console.log(password);
+
+});
+
